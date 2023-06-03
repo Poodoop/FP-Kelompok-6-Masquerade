f30a0484f9695473fedf204a298944ab84abe410
Poodoop <dickyaristyo43@gmail.com>
2023-06-03 22:59:29 +0700

    test 2

--- Masquerade/Assets/Scripts/Projectile.cs
+++ Masquerade/Assets/Scripts/Projectile.cs
@@ -6,16 +6,31 @@ public class Projectile : MonoBehaviour
 {
     public float speed;
     public float lifeTime;
+    public int damage;
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, lifeTime);    
+        Invoke("DestroyProjectile", lifeTime);    
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            collision.GetComponent<Enemy>().Damage(damage);
+            DestroyProjectile();
+        }
     }
 }
